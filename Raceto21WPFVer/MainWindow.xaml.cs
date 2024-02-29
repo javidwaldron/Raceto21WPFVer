5af7cdb961dfb40fa77a1bfafc4cd3e85992b2b3
javidwaldron <javidwaldron@gmail.com>
2024-02-29 12:15:37 -0500

    Successful preview press down removes prompt

--- Raceto21WPFVer/MainWindow.xaml.cs
+++ Raceto21WPFVer/MainWindow.xaml.cs
@@ -2,17 +2,30 @@
 
 namespace Raceto21WPFVer
 {
+    
 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+
+
             InitializeComponent();
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             System.Environment.Exit(0);
+        }
+
+        private void enterButtonMain_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void howManyPlayers_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            howManyPlayers.Text = "";
         }
     }
 }

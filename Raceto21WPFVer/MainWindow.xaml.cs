a223528fdabd44b8854bd65650efc99b0f848de8
javidwaldron <javidwaldron@gmail.com>
2024-03-01 18:42:41 -0500

    Ok Took forever, added a way to store names and then to also store each name to the players in the list by hitting the ready button, need to trigger its visibility once all players are in

--- Raceto21WPFVer/MainWindow.xaml.cs
+++ Raceto21WPFVer/MainWindow.xaml.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 
 
 namespace Raceto21WPFVer
 {
-    
+
 
     public partial class MainWindow : Window
     {
-       
+
         Game game = new Game();
 
         public int numberOfPlayers;
+        public int nameenteredcount;
+        public bool allPlayersIn = false;
+
         List<Player> players = new List<Player>();
         public MainWindow()
         {
+
             InitializeComponent();
-            game.Setup(numberOfPlayers);  
-        
+
+            
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -26,47 +31,54 @@ namespace Raceto21WPFVer
             System.Environment.Exit(0);
         }
 
-         private void howManyPlayers_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void howManyPlayers_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             howManyPlayers.Text = "";
         }
 
-        
-        public void enterButtonMain_Click(object sender, RoutedEventArgs e)
+
+        private void howManyPlayers_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            
-            //Taking only acceptable answers, limited player count to 2-4
-
-            if (int.TryParse(howManyPlayers.Text, out numberOfPlayers) == false
-                  || numberOfPlayers < 0 || numberOfPlayers > 4)
+            if (e.Key == System.Windows.Input.Key.Return)
             {
 
-                howManyPlayers.Text = "Please enter a valid number between 2 and 4";
-                
-
-            }
-            //Removes Question Box and Enter Button 
-            else
-            {
-                AskBox.Text = "There are " + numberOfPlayers + " people playing!";
-                numberOfPlayers = int.Parse(howManyPlayers.Text);
-                howManyPlayers.Visibility = Visibility.Hidden;
-                enterButtonMain.Visibility = Visibility.Hidden;
 
 
-                for (int i = 0; i < numberOfPlayers; i++)
+                if (int.TryParse(howManyPlayers.Text, out numberOfPlayers) == false
+                      || numberOfPlayers < 0 || numberOfPlayers > 4)
                 {
 
-                    players.Add(new Player());
+                    howManyPlayers.Text = "Please enter a valid number between 2 and 4";
+
+
                 }
-               
-                PlayerNames();
+                //Removes Question Box and Enter Button 
+                else
+                {
+                    AskBox.Text = "There are " + numberOfPlayers + " people playing!";
+                    numberOfPlayers = int.Parse(howManyPlayers.Text);
+                    howManyPlayers.Visibility = Visibility.Hidden;
 
 
+                    for (int i = 0; i < numberOfPlayers; i++)
+                    {
+
+                        players.Add(new Player());
+                   
+                    }
+
+                    
+                    PlayerNames();
+
+                    
+
+                }
 
             }
-               
+
         }
+
+        // Enables player placement visibility and prompts name entering ask boxes
         public void PlayerNames()
         {
 
@@ -75,38 +87,184 @@ namespace Raceto21WPFVer
                 case 2:
                     PlayerBPlacementDeck.Visibility = Visibility.Visible;
                     PlayerNamePlacementB.Visibility = Visibility.Visible;
+                    PlayerNameBAskBox.Visibility = Visibility.Visible;
                     PlayerDPlacementDeck.Visibility = Visibility.Visible;
                     PlayerNamePlacementD.Visibility = Visibility.Visible;
+                    PlayerNameDAskBox.Visibility = Visibility.Visible;
                     break;
                 case 3:
-                    PlayerBPlacementDeck.Visibility= Visibility.Visible;
-                    PlayerNamePlacementB.Visibility= Visibility.Visible;
-                    PlayerCPlacementDeck.Visibility= Visibility.Visible;
-                    PlayerNamePlacementC.Visibility= Visibility.Visible;
-                    PlayerDPlacementDeck.Visibility= Visibility.Visible;
-                    PlayerNamePlacementD.Visibility= Visibility.Visible;
+                    PlayerBPlacementDeck.Visibility = Visibility.Visible;
+                    PlayerNamePlacementB.Visibility = Visibility.Visible;
+                    PlayerNameBAskBox.Visibility = Visibility.Visible;
+                    PlayerCPlacementDeck.Visibility = Visibility.Visible;
+                    PlayerNamePlacementC.Visibility = Visibility.Visible;
+                    PlayerNameCAskBox.Visibility = Visibility.Visible;
+                    PlayerDPlacementDeck.Visibility = Visibility.Visible;
+                    PlayerNamePlacementD.Visibility = Visibility.Visible;
+                    PlayerNameDAskBox.Visibility = Visibility.Visible;
                     break;
                 case 4:
                     PlayerAPlacementDeck.Visibility = Visibility.Visible;
                     PlayerNamePlacementA.Visibility = Visibility.Visible;
+                    PlayerNameAAskBox.Visibility = Visibility.Visible;
                     PlayerBPlacementDeck.Visibility = Visibility.Visible;
                     PlayerNamePlacementB.Visibility = Visibility.Visible;
+                    PlayerNameBAskBox.Visibility = Visibility.Visible;
                     PlayerDPlacementDeck.Visibility = Visibility.Visible;
                     PlayerNamePlacementD.Visibility = Visibility.Visible;
+                    PlayerNameDAskBox.Visibility = Visibility.Visible;
                     PlayerEPlacementDeck.Visibility = Visibility.Visible;
                     PlayerNamePlacementE.Visibility = Visibility.Visible;
+                    PlayerNameEAskBox.Visibility = Visibility.Visible;
                     break;
 
             }
 
 
-
         }
-
+        // Move this, this restarts application for new game
         private void MenuItemNewGame_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
             Application.Current.Shutdown();
+        }
+
+
+        // Allows players to enter name after hitting enter
+
+        private void PlayerNameAAskBox_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            PlayerNameAAskBox.Text = "";
+
+
+        }
+        private void PlayerNameAAskBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
+                PlayerNamePlacementA.Text = PlayerNameAAskBox.Text;
+                PlayerNameAAskBox.Visibility = Visibility.Hidden;
+                nameenteredcount++;
+                Debug.WriteLine("Look for this" + nameenteredcount + "is it this yet" + numberOfPlayers + "");
+            }
+
+        }
+        private void PlayerNameBAskBox_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            PlayerNameBAskBox.Text = "";
+
+
+        }
+        private void PlayerNameBAskBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
+                PlayerNamePlacementB.Text = PlayerNameBAskBox.Text;
+                PlayerNameBAskBox.Visibility = Visibility.Hidden;
+                nameenteredcount++;
+                Debug.WriteLine("Look for this" + nameenteredcount + "is it this yet" + numberOfPlayers + "");
+            }
+
+
+        }
+        private void PlayerNameCAskBox_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            PlayerNameCAskBox.Text = "";
+
+
+        }
+        private void PlayerNameCAskBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
+                PlayerNamePlacementC.Text = PlayerNameCAskBox.Text;
+                PlayerNameCAskBox.Visibility = Visibility.Hidden;
+                nameenteredcount++;
+                Debug.WriteLine("Look for this" + nameenteredcount + "is it this yet" + numberOfPlayers + "");
+            }
+
+
+        }
+        private void PlayerNameDAskBox_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            PlayerNameDAskBox.Text = "";
+
+
+        }
+        private void PlayerNameDAskBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
+                PlayerNamePlacementD.Text = PlayerNameDAskBox.Text;
+                PlayerNameDAskBox.Visibility = Visibility.Hidden;
+                nameenteredcount++;
+                Debug.WriteLine("Look for this" + nameenteredcount + "is it this yet" + numberOfPlayers + "");
+            }
+
+
+        }
+
+        private void PlayerNameEAskBox_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            PlayerNameEAskBox.Text = "";
+
+
+        }
+        private void PlayerNameEAskBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
+                PlayerNamePlacementE.Text = PlayerNameEAskBox.Text;
+                PlayerNameEAskBox.Visibility = Visibility.Hidden;
+                nameenteredcount++;
+            }
+
+        }
+
+        public void AssignPlayers()
+        {
+
+           
+                switch (numberOfPlayers)
+                {
+                    case 2:
+                        players[0].name = PlayerNamePlacementB.Text;
+                        players[1].name = PlayerNamePlacementD.Text;
+                        Player_1_Score_and_Holder.Text = "Player 1 is " + players[0].name + " with a score of ";
+                        Player_2_Score_and_Holder.Text = "Player 2 is " + players[1].name + " with a score of ";
+                        break;
+
+                    case 3:
+                        players[0].name = PlayerNamePlacementB.Text;
+                        players[1].name = PlayerNamePlacementC.Text;
+                        players[2].name = PlayerNamePlacementD.Text;
+                        Player_1_Score_and_Holder.Text = "Player 1 is " + players[0].name + " with a score of ";
+                        Player_2_Score_and_Holder.Text = "Player 2 is " + players[1].name + " with a score of ";
+                        Player_3_Score_and_Holder.Text = "Player 3 is " + players[2].name + " with a score of ";
+                        break;
+
+                    case 4:
+                        players[0].name = PlayerNamePlacementA.Text;
+                        players[1].name = PlayerNamePlacementB.Text;
+                        players[2].name = PlayerNamePlacementD.Text;
+                        players[3].name = PlayerNamePlacementE.Text;
+                        Player_1_Score_and_Holder.Text = "Player 1 is " + players[0].name + " with a score of ";
+                        Player_2_Score_and_Holder.Text = "Player 2 is " + players[1].name + " with a score of ";
+                        Player_3_Score_and_Holder.Text = "Player 3 is " + players[2].name + " with a score of ";
+                        Player_3_Score_and_Holder.Text = "Player 3 is " + players[3].name + " with a score of ";
+                    break;
+
+                }
+            
+            
+
+        }
+
+        private void ReadyButton_Click(object sender, RoutedEventArgs e)
+        {
+            AssignPlayers();
         }
     }
 }
